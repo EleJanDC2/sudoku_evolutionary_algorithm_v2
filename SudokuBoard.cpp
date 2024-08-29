87255d8e3691eee12857745d900558497621bf9e
Jan Czaja | EleJanDC2 <elejandc.2@gmail.com>
2024-08-29 18:11:00 +0200

    Creating a Sudoku Board constructor from an array of ints

--- SudokuBoard.cpp
+++ SudokuBoard.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "SudokuBoard.h"
+
+#include <algorithm>
+
+SudokuBoard::SudokuBoard(int *init_board) {
+    std::copy(init_board, init_board + 81, &_board[0][0]);
+
+    this->_empty_spaces = static_cast<int> (std::count(init_board, init_board + 81, 0));
+}
--- SudokuBoard.h
+++ SudokuBoard.h
@@ -4,11 +4,21 @@
 
 #ifndef SUDOKUBOARD_H
 #define SUDOKUBOARD_H
-
+#include <array>
+#include <bitset>
 
 
 class SudokuBoard {
+public:
+    SudokuBoard(int * init_board);
 
+private:
+    int _board[9][9];
+    std::array<std::bitset<9>, 9> _row_contains;
+    std::array<std::bitset<9>, 9> _col_contains;
+    std::array<std::bitset<9>, 9> _block_contains;
+
+    int _empty_spaces = 81;
 };
 
 
